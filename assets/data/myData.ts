cfe16c53a19f73f2dbfbf57bc207572fbee3b619
koharshi <46941627+koharshi@users.noreply.github.com>
2020-08-07 23:52:22 -0400

    Add files via upload

---
+++ assets/data/myData.ts
@@ -0,0 +1,9 @@
+import { YourLoginName } from '../../app/yourLoginName';
+
+export const MYINFORMATION : YourLoginName = {
+    StudentNumber: 991488484,
+    yourName:  "Harshit Kohli",
+    yourLoginName: "koharshi",
+    yourCampus: "Davis",
+    assignmentTitle: "Assignment 4"
+};
